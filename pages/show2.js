8c1dea49b7040f426f40bfcfa7ed78a2f1658dfd
Johannes Langosch <johannes.langosch@googlemail.com>
2022-12-07 18:51:34 +0100

    12 show2 added dropdown sign

--- pages/show2.js
+++ pages/show2.js
@@ -29,16 +29,45 @@ function show() {
             <ul>Link 1</ul>
             <ul
               className="relative"
-              onMouseEnter={() => setIsShown(true)}
-              onMouseLeave={() => setIsShown(false)}
+              // onMouseEnter={() => setIsShown(true)}
+              // onMouseLeave={() => setIsShown(false)}
               onClick={() => setIsShown(!isShown)}>
               {!isShown ? (
-                <span className="w-full text-center">
-                  Link 2
-                </span>
+                <div className="">
+                  <span className="w-full text-center flex flex-row justify-center">
+                    Link 2
+                    <svg
+                      className=""
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      class="w-6 h-6">
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+                      />
+                    </svg>
+                  </span>
+                </div>
               ) : (
-                <span className="w-full underline text-center">
+                <span className="w-full underline text-center flex flex-row justify-center">
                   Link 2
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="w-6 h-6">
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M4.5 15.75l7.5-7.5 7.5 7.5"
+                    />
+                  </svg>
                 </span>
               )}
               {isShown && (
