565d858a4ddb90ddd060101644196e23a87b96dd
Johannes Langosch <johannes.langosch@googlemail.com>
2022-12-26 09:44:21 +0100

    17 mobile version links

--- pages/test.js
+++ pages/test.js
@@ -4,13 +4,16 @@ import { useState } from "react";
 
 function test() {
   const [showMenue, setShowMenue] = useState(false);
-  const [showDrop, setShowDrop] = useState(false);
+  const [showDrop1, setShowDrop1] = useState(false);
+  const [showDrop2, setShowDrop2] = useState(false);
+  const [showDrop3, setShowDrop3] = useState(false);
+  const [showDrop4, setShowDrop4] = useState(false);
   return (
     <div>
       <div className="h-[400vh] bg-green-500">
         <div className="flex md:justify-between items-center first-letter:p-4 bg-white pl-4 md:pr-36 mt-0 space-x-4 sticky top-0 shadow-xl">
           <Image
-            className="flex items-center hover:cursor-pointer mt-0 z-50"
+            className="flex items-center hover:cursor-pointer mt-0 z-50 select-none"
             height={100}
             width={175}
             src={"/src/image.png"}
@@ -25,7 +28,7 @@ function test() {
               viewBox="0 0 24 24"
               stroke-width="1.5"
               stroke="currentColor"
-              class="w-6 h-6">
+              className="w-6 h-6">
               <path
                 stroke-linecap="round"
                 stroke-linejoin="round"
@@ -120,25 +123,27 @@ function test() {
           </div>
         </div>
 
+        {/* Links mobile view */}
         {showMenue && (
-          <div className="flex flex-col md:hidden w-full md:space-y-0 md:space-x-4 space-y-4 bg-green-600 rounded-lg pl-[45%]">
-            <ul className="w-auto">Link 1</ul>
+          <div className="flex flex-col md:hidden w-full md:space-y-0 md:space-x-4 space-y-4 rounded-lg pl-20 text-[#4c7298] select-none">
+            {/* link without dropdown */}
+            <ul className="w-auto mt-4">Start</ul>
+            {/* link with dropdown1 */}
             <ul
               className="relative"
-              onClick={() => setShowDrop(!showDrop)}>
+              onClick={() => setShowDrop1(!showDrop1)}>
               {/* dropdown */}
-              {!showDrop ? (
+              {!showDrop1 ? (
                 <div className="w-auto">
                   <span className="w-full text-center flex flex-row justify-start">
-                    Link 2
+                    Wissen
                     <svg
-                      className=""
                       xmlns="http://www.w3.org/2000/svg"
                       fill="none"
                       viewBox="0 0 24 24"
                       stroke-width="1.5"
                       stroke="currentColor"
-                      class="w-6 h-6">
+                      className="w-6 h-6 ml-2">
                       <path
                         stroke-linecap="round"
                         stroke-linejoin="round"
@@ -149,14 +154,14 @@ function test() {
                 </div>
               ) : (
                 <span className="w-full underline text-center flex flex-row justify-start">
-                  Link 2
+                  Wissen
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke-width="1.5"
                     stroke="currentColor"
-                    class="w-6 h-6">
+                    className="w-6 h-6 ml-2">
                     <path
                       stroke-linecap="round"
                       stroke-linejoin="round"
@@ -166,21 +171,212 @@ function test() {
                 </span>
               )}
               {/* dropdown */}
-              {showDrop && (
-                <div className="rounded-lg">
+              {showDrop1 && (
+                <div className="rounded-lg pl-2">
                   <ul className="my-2">
                     <a href="/">
-                      <span>Link A</span>
+                      <span>Notfallinfos</span>
                     </a>
                   </ul>
-                  <ul className="mb-2">
-                    <a href="/">Link B</a>
+                  <ul className="">
+                    <a href="/">Blog</a>
                   </ul>
-                  <ul className="mb-6">Link C</ul>
                 </div>
               )}
             </ul>
-            <ul>Link 3</ul>
+            {/* link without dropdown */}
+            <ul>Honorar</ul>
+            {/* link with dropdown2 */}
+            <ul
+              className="relative"
+              onClick={() => setShowDrop2(!showDrop2)}>
+              {/* dropdown */}
+              {!showDrop2 ? (
+                <div className="w-auto">
+                  <span className="w-full text-center flex flex-row justify-start">
+                    Angebot
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      className="w-6 h-6 ml-2">
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+                      />
+                    </svg>
+                  </span>
+                </div>
+              ) : (
+                <span className="w-full underline text-center flex flex-row justify-start">
+                  Angebot
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6 ml-2">
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M4.5 15.75l7.5-7.5 7.5 7.5"
+                    />
+                  </svg>
+                </span>
+              )}
+              {/* dropdown */}
+              {showDrop2 && (
+                <div className="rounded-lg pl-2">
+                  <ul className="my-2">
+                    <a href="/">
+                      <span>Übersicht</span>
+                    </a>
+                  </ul>
+                  <ul className="mb-2">
+                    <a href="/">Klopfen gegen Angst</a>
+                  </ul>
+                  <ul className="mb-2">
+                    <a href="/">
+                      Unterstützer-Rettungsring
+                    </a>
+                  </ul>
+                  <ul className="mb-2">
+                    <a href="/">
+                      Naturcoaching Selbstführung
+                    </a>
+                  </ul>
+                  <ul className="mb-2">
+                    <a href="/">Coaching</a>
+                  </ul>
+                  <ul className="mb-2">
+                    <a href="/">
+                      piKVT kognitive Verhaltenstherapie
+                    </a>
+                  </ul>
+                </div>
+              )}
+            </ul>
+
+            {/* link with dropdown3 */}
+            <ul
+              className="relative"
+              onClick={() => setShowDrop3(!showDrop3)}>
+              {/* dropdown */}
+              {!showDrop3 ? (
+                <div className="w-auto">
+                  <span className="w-full text-center flex flex-row justify-start">
+                    Über...
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      className="w-6 h-6 ml-2">
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+                      />
+                    </svg>
+                  </span>
+                </div>
+              ) : (
+                <span className="w-full underline text-center flex flex-row justify-start">
+                  Über...
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6 ml-2">
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M4.5 15.75l7.5-7.5 7.5 7.5"
+                    />
+                  </svg>
+                </span>
+              )}
+              {/* dropdown */}
+              {showDrop3 && (
+                <div className="rounded-lg pl-2">
+                  <ul className="my-2">
+                    <a href="/">
+                      <span>...Sie</span>
+                    </a>
+                  </ul>
+                  <ul className="mb-2">
+                    <a href="/">...mich</a>
+                  </ul>
+                </div>
+              )}
+            </ul>
+
+            {/* link with dropdown4 */}
+            <ul
+              className="relative"
+              onClick={() => setShowDrop4(!showDrop4)}>
+              {/* dropdown */}
+              {!showDrop4 ? (
+                <div className="w-auto">
+                  <span className="w-full text-center flex flex-row justify-start">
+                    Kontakt
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      className="w-6 h-6 ml-2">
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+                      />
+                    </svg>
+                  </span>
+                </div>
+              ) : (
+                <span className="w-full underline text-center flex flex-row justify-start">
+                  Kontakt
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6 ml-2">
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M4.5 15.75l7.5-7.5 7.5 7.5"
+                    />
+                  </svg>
+                </span>
+              )}
+              {/* dropdown */}
+              {showDrop4 && (
+                <div className="rounded-lg pl-2">
+                  <ul className="my-2">
+                    <a href="/">
+                      <span>Datenschutz persönlich</span>
+                    </a>
+                  </ul>
+                  <ul className="mb-2">
+                    <a href="/">Datenschutz juristisch</a>
+                  </ul>
+                  <ul className="mb-2">
+                    <a href="/">Impressum</a>
+                  </ul>
+                </div>
+              )}
+            </ul>
           </div>
         )}
       </div>
